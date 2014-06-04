5910bcbbdc38d64f6443514c4aa62bf7d9553d0d
jeremyfelt <jeremy.felt@gmail.com>
2014-06-04 16:01:17 -0700

    Introduce TOC for jQuery and basic TOC configuration
    Adds toc - a jQuery Table of Contents Plugin and intiiates it for
    use on the page

---
+++ js/wsuwp-toc-generator.js
@@ -0,0 +1,4 @@
+(function($){
+	$('body' ).append('<div id="toc"></div>');
+	$('#toc' ).toc();
+}(jQuery));
--- wsuwp-toc-generator.php
+++ wsuwp-toc-generator.php
@@ -1,9 +1,27 @@
 <?php
 /*
 Plugin Name: WSUWP Table of Contents Generator
-Version: 0.1
+Version: 0.1.0
 Plugin URI: http://web.wsu.edu
 Description: Generates a DOM element containing the table of contents for a long page.
 Author: washingtonstateuniversity, jeremyfelt
 Author URI: http://web.wsu.edu
 */
+
+class WSUWP_TOC_Generator {
+
+	/**
+	 * @var string Current version of this plugin.
+	 */
+	var $plugin_version = '0.1.0';
+
+	public function __construct() {
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
+	}
+
+	public function enqueue_scripts() {
+		wp_enqueue_script( 'toc-jquery', plugins_url( 'js/toc.min.js', __FILE__ ), array( 'jquery' ), $this->plugin_version, true );
+		wp_enqueue_script( 'wsuwp-toc-generator', plugins_url( 'js/wsuwp-toc-generator.js', __FILE__ ), array( 'toc-jquery', 'jquery' ), $this->plugin_version, true );
+	}
+}
+new WSUWP_TOC_Generator();
