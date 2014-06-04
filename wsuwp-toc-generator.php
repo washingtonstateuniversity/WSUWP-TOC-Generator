a5d7b724e9106f2839d37f0fc5e8af3db0bd23ee
jeremyfelt <jeremy.felt@gmail.com>
2014-06-04 15:23:45 -0700

    Add initial plugin files

---
+++ wsuwp-toc-generator.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: WSUWP Table of Contents Generator
+Version: 0.1
+Plugin URI: http://web.wsu.edu
+Description: Generates a DOM element containing the table of contents for a long page.
+Author: washingtonstateuniversity, jeremyfelt
+Author URI: http://web.wsu.edu
+*/
