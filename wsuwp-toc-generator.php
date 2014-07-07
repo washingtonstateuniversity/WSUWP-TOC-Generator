93bb7c6d60d966672707b216ee7c4c96928ab800
jeremyfelt <jeremy.felt@gmail.com>
2014-07-07 10:45:34 -0700

    Allow for the TOC to be output in place or at the bottom of the page

--- wsuwp-toc-generator.php
+++ wsuwp-toc-generator.php
@@ -25,9 +25,30 @@ class WSUWP_TOC_Generator {
 	/**
 	 * Enqueue the scripts used to display the table of contents.
 	 */
-	public function display_toc() {
+	public function display_toc( $attributes ) {
+		$attributes = shortcode_atts( array( 'position' => 'content' ), $attributes );
+
+		if ( ! in_array( $attributes['position'], array( 'content', 'bottom' ) ) ) {
+			$attributes['position'] = 'content';
+		}
+
 		wp_enqueue_script( 'toc-jquery', plugins_url( 'js/toc.min.js', __FILE__ ), array( 'jquery' ), $this->plugin_version, true );
 		wp_enqueue_script( 'wsuwp-toc-generator', plugins_url( 'js/wsuwp-toc-generator.js', __FILE__ ), array( 'toc-jquery', 'jquery' ), $this->plugin_version, true );
+
+		if ( 'content' === $attributes['position'] ) {
+			return '<div id="toc"></div>';
+		}
+
+		add_action( 'wp_footer', array( $this, 'footer_display_toc' ) );
+
+		return '';
+	}
+
+	/**
+	 * Display the TOC element at the end of the page for custom positioning.
+	 */
+	public function footer_display_toc() {
+		echo '<div id="toc"></div>';
 	}
 }
 new WSUWP_TOC_Generator();
